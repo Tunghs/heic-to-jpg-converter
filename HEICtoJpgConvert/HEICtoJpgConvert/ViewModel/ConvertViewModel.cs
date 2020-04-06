907c097e7738a3e434bafa8d440831ce6a0641e7
이성현 <dljdgus1211@naver.com>
2020-04-06 23:01:47 +0900

    multi selected items value

--- HEICtoJpgConvert/HEICtoJpgConvert/ViewModel/ConvertViewModel.cs
+++ HEICtoJpgConvert/HEICtoJpgConvert/ViewModel/ConvertViewModel.cs
@@ -24,7 +24,7 @@ namespace HEICtoJpgConvert.ViewModel
             get { return _collectionFileList; }
             set { _collectionFileList = value; }
         }
-        private List<string> _selectedListBoxItems = new List<string>();
+        private  _selectedListBoxItems = new List<string>();
         public List<string> SelectedListBoxItems
         {
             get { return _selectedListBoxItems; }
