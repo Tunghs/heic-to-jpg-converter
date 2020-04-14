2c25b812528b50ab430a9fb6c4dafb07dc26b22f
이성현 <dljdgus1211@naver.com>
2020-04-14 22:09:12 +0900

    get listview items property

--- HEICtoJpgConvert/HEICtoJpgConvert/ViewModel/ConvertViewModel.cs
+++ HEICtoJpgConvert/HEICtoJpgConvert/ViewModel/ConvertViewModel.cs
@@ -2,10 +2,13 @@
 using GalaSoft.MvvmLight.Command;
 using System.IO;
 using System.Windows;
-using ImageMagick;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+using ImageMagick;
 
 namespace HEICtoJpgConvert.ViewModel
 {
@@ -36,26 +39,33 @@ namespace HEICtoJpgConvert.ViewModel
             get { return _saveDirControlEnabled; }
             set { _saveDirControlEnabled = value; RaisePropertyChanged("SaveDirControlEnabled"); }
         }
-        //private  _selectedListBoxItems = new List<string>();
-        //public List<string> SelectedListBoxItems
-        //{
-        //    get { return _selectedListBoxItems; }
-        //    set { _selectedListBoxItems = value; RaisePropertyChanged("SelectedListBoxItems"); }
-        //}
+        private List<string> _selectedListViewItems = new List<string>();
+        public List<string> SelectedListViewItems
+        {
+            get { return _selectedListViewItems; }
+            set { _selectedListViewItems = value; RaisePropertyChanged("SelectedListViewItems"); }
+        }
         #endregion
 
         #region Command
         public RelayCommand<object> ButtonClickCommand { get; private set; }
         public RelayCommand<object> CheckBoxClickCommand { get; private set; }
+        public RelayCommand<IList> ListViewSelectCommand { get; private set; }
 
         private void InitRelayCommand()
         {
             ButtonClickCommand = new RelayCommand<object>(OnButtonClick);
-            CheckBoxClickCommand = new RelayCommand<object>(OnCheckBoxClick);
+            CheckBoxClickCommand = new RelayCommand<object>(CheckBox_OnClick);
+            ListViewSelectCommand = new RelayCommand<IList>(ListViewSelect);
         }
 
         #region CommandAction
-        private void OnCheckBoxClick(object param)
+        private void ListViewSelect(IList param)
+        {
+            SelectedListViewItems = param.Cast<string>().ToList();
+        }
+
+        private void CheckBox_OnClick(object param)
         {
             bool isCheck;
 
@@ -104,7 +114,8 @@ namespace HEICtoJpgConvert.ViewModel
 
         private void DeleteFile_OnClick()
         {
-            MessageBox.Show(SaveDirControlEnabled.ToString());
+            // MessageBox.Show(SaveDirControlEnabled.ToString());
+            MessageBox.Show(SelectedListViewItems[0].ToString());
         }
 
         /// <summary>
