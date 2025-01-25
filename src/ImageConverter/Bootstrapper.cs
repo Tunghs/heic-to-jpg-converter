9176f9c3d0435f209b6b7c6393c4b1e060da3513
Tunghs <juk08843@gmail.com>
2025-01-26 00:08:26 +0900

    Feat: "Bootstrapper 코드 추가"

--- src/ImageConverter/Bootstrapper.cs
+++ src/ImageConverter/Bootstrapper.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+using Wpf.Ui;
 
 namespace ImageConverter
 {
-    internal class Bootstrapper
+    public class Bootstrapper
     {
+        public Bootstrapper()
+        {
+            var services = ConfigureServices();
+            Ioc.Default.ConfigureServices(services);
+        }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        /// <returns></returns>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+
+            // WPF-UI Snackbar service 등록
+            services.AddSingleton<ISnackbarService, SnackbarService>();
+
+            // Viewer ViewModels
+
+
+            return services.BuildServiceProvider();
+        }
     }
 }
