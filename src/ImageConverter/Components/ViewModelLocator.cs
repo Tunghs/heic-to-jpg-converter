2f295f672a238bde8b079e58434215ecee62dac0
Tunghs <juk08843@gmail.com>
2025-01-29 21:34:48 +0900

    Feat: "ViewModelLocator 추가"

---
+++ src/ImageConverter/Components/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using CommunityToolkit.Mvvm.DependencyInjection;
+
+using ImageConverter.ViewModel;
+
+namespace ImageConverter.Components
+{
+    public sealed class ViewModelLocator
+    {
+        public ConverterViewModel? ConverterViewModel
+            => Ioc.Default.GetService<ConverterViewModel>();
+    }
+}
